ce48d25ad0070044db50391690527438f96b6883
allenjustinblanco <blanco.allenjustin@gmail.com>
2021-09-15 15:49:18 -0500

    Add base logic for evolving cell

--- REVISIONS.md
+++ REVISIONS.md
@@ -1,2 +1,6 @@
 Are there any sections that you would have done differently with
 hindsight? Anything that you'd like to refactor given more time?
+
+If I were to approach the section where I was checking neighbors in the matrix I would have tried to find a simpler/cleaner solution. Perhaps utilizing higher order functions. I also would have refactored my return value for the neighbors method thus making my conditionals more clean. 
+
+If given more time I would try to minimize the nesting of my conditional statements. Maybe by using a switch statement, ternary operators, or attempting other conditions. I would have also spent more time writing tests and would have taken a more tdd approach to pass the specs prior to implementing any functionality. 
--- src/app/components/cellular-automata/cellular-automata.component.ts
+++ src/app/components/cellular-automata/cellular-automata.component.ts
@@ -28,28 +28,84 @@ export class CellularAutomataComponent implements OnInit {
   }
 
   cellularAutomation(arr: Array<any>) : void {
+    let evolvedArr = new Array(arr.length).fill(new Array(10));
+    console.log('evolvedArr', evolvedArr);
     // iterate over 2d arr
     for(let i = 0; i < arr.length; i++) {
       for(let j = 0; j < arr[i].length; j++) {
-         console.log(arr[i][j]);
+        let neighbors = this.neighbors(arr, [i], [j])
+        // console.log(neighbors)
+        this.evolveCell(neighbors, arr, i, j, evolvedArr);
+
       }
+
    }
+  }
+
+
+  getCell(arr: any, y:number , x: number) {
+    var NO_VALUE = null;
+    var value, hasValue;
+    
+    try {
+      hasValue = arr[y][x] !== undefined;
+      value    = hasValue?  arr[y][x] : NO_VALUE;
+    } catch(e) {
+      value    = NO_VALUE;
+    }
+    console.log(value)
+    return value;
+  }
+  
+  neighbors(arr: any, y: any , x: any) {
+    return {
+      up:        this.getCell(arr, y-1, x),
+      right:     this.getCell(arr, y,   x+1),
+      down:      this.getCell(arr, y+1, x),
+      left:      this.getCell(arr, y,   x-1),
+    }
+  }
+
+  evolveCell(neighbors: any, arr: any , i: any , j: any ,evolvedArr: any) {
+
     // empty
     // exactly two adult neighbors turn into 1 comment reproduction
     // otherwise empty
+    if (arr[i][j] == (0 || null)) {
+        
+      if (neighbors.down != null && neighbors.up != null && neighbors.right != null && neighbors.left != null) {
+        
+      } else {
 
+      }
+    
     // 1 newborn
     // >= 5 total neighbors make empty comment overcrowding
     // <= 1 total neighbors make empty comment isolation
     // otherwise make 2 comment growing up
+    } else if (arr[i][j] == 1) {
+      if (neighbors.down != null && neighbors.up != null && neighbors.right != null && neighbors.left != null) {
+        evolvedArr[i][j] = null;
+        console.log("Overcrowding")
+      } else if (neighbors.down == null || neighbors.up == null || neighbors.right == null || neighbors.left == null) {
+        evolvedArr[i][j] = null;
+        console.log("Isolation")
+      } else {
+        evolvedArr[i][j] = 2;
+        console.log("Isolation")
+      }
 
     // 2 adult
     // >=3 total neighbors make empty comment overcrowding
     // zero neighbors make empty comment isolation
     // otherwise make 3 comment Aging
+    } else if (arr[i][j] == 2) {
+
 
     // 3 senior
     // All conditions make empty comment "natural causes"
+    } else if (arr[i][j] == 3) {
+      evolvedArr[i][j] = 0;
+    }
   }
-
 }
