26aebd23c83225a7935eb497c79df2212ef2df3c
allenjustinblanco <blanco.allenjustin@gmail.com>
2021-09-15 14:40:14 -0500

    Add nested loop to iterate over 2d array

--- src/app/components/cellular-automata/cellular-automata.component.ts
+++ src/app/components/cellular-automata/cellular-automata.component.ts
@@ -7,14 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CellularAutomataComponent implements OnInit {
 
+  arr = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    let arr = [
+      [0,0,2,2,0,0,0,0,0,0],
+      [0,0,0,0,1,3,1,0,0,0],
+      [0,0,0,2,0,3,0,0,1,3],
+      [0,0,0,0,1,3,0,0,0,3],
+      [0,2,2,0,0,0,1,3,1,0],
+      [0,0,0,0,0,2,2,0,0,0],
+      [0,0,2,0,2,0,0,0,0,0],
+      [0,0,0,0,2,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+    ];
+    this.cellularAutomation(arr);
   }
 
-  cellularAutomation(arr: []) {
+  cellularAutomation(arr: Array<any>) : void {
     // iterate over 2d arr
-
+    for(let i = 0; i < arr.length; i++) {
+      for(let j = 0; j < arr[i].length; j++) {
+         console.log(arr[i][j]);
+      }
+   }
     // empty
     // exactly two adult neighbors turn into 1 comment reproduction
     // otherwise empty
