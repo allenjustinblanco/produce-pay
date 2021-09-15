a47e6ca57aab4547c926ea6a5adbac382a57ab73
allenjustinblanco <blanco.allenjustin@gmail.com>
2021-09-15 14:33:25 -0500

    Add pseudo code to clarify thought process and conditional logic solution

--- src/app/components/cellular-automata/cellular-automata.component.ts
+++ src/app/components/cellular-automata/cellular-automata.component.ts
@@ -12,4 +12,25 @@ export class CellularAutomataComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  cellularAutomation(arr: []) {
+    // iterate over 2d arr
+
+    // empty
+    // exactly two adult neighbors turn into 1 comment reproduction
+    // otherwise empty
+
+    // 1 newborn
+    // >= 5 total neighbors make empty comment overcrowding
+    // <= 1 total neighbors make empty comment isolation
+    // otherwise make 2 comment growing up
+
+    // 2 adult
+    // >=3 total neighbors make empty comment overcrowding
+    // zero neighbors make empty comment isolation
+    // otherwise make 3 comment Aging
+
+    // 3 senior
+    // All conditions make empty comment "natural causes"
+  }
+
 }
